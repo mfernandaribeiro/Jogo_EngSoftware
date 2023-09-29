993156354c14e220a4c450000c02d910698bcabd
Maria Fernanda Ribeiro <mariafernandarbpc@gmail.com>
2023-09-29 17:42:03 -0300

    add menu

--- src/teste.py
+++ src/teste.py
@@ -2,7 +2,7 @@ import pygame
 from PyQt5 import uic, QtWidgets
 
 app = QtWidgets.QApplication([])
-login_screen = uic.loadUi("assets/login.ui")
+login_screen = uic.loadUi("assets/menu.ui")
 
 login_screen.show()
 app.exec()
